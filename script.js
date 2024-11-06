27bb39c1cec8add0b08be0e3107c4bfe84e6fc71
attacksalt <tejasodg@gmail.com>
2024-11-06 12:45:31 +0530

    Add functions for hover and color effect

--- script.js
+++ script.js
@@ -13,7 +13,41 @@ function makeGrid(div) {
         el.style.width = CONTAINER_HEIGHT / 16 + "px";
         el.style.height = CONTAINER_HEIGHT / 16 + "px";
         div.appendChild(el);
+        addHoverEffect(el);
+        addColorEffect(div);
     }
+}
+
+function addColorEffect(container) {
+    let isHolding = false;
+    container.addEventListener('mousedown', () =>{
+        isHolding = true;
+        //console.log("rf1:", isHolding);
+    });
+    container.addEventListener('mouseup', () => {
+        isHolding = false;
+        //console.log("rf2:", isHolding);
+    })
+    container.addEventListener('mouseover', e => {
+        //console.log("rf4:", e.target, isHolding)
+        if (e.target.classList.contains('block') && isHolding) {
+            color(e.target);
+            //console.log("rf3:", isHolding);
+        }
+    })
+}
+
+function addHoverEffect(pixel) {
+    pixel.addEventListener('mouseover', ()=>{hover(pixel)});
+    pixel.addEventListener('mouseout', ()=>{hover(pixel)});
+}
+
+function hover(pixel) {
+    pixel.classList.toggle('hovered');
+}
+
+function color(pixel) {
+    pixel.classList.add('colored');
 }
 
 // vars and stuff
